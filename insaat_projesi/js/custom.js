71422728d8ba00a0217344d506b67ddb6b23e425
meviz <m.eviz@outlook.com>
2016-11-29 23:15:24 +0200

    Add files via upload

--- insaat_projesi/js/custom.js
+++ insaat_projesi/js/custom.js
@@ -31,4 +31,54 @@ $( document ).ready(function(){
 		}
 		
 	});
+	
+	//if (secDuz()=="on"){
+		$(".pozunAnalizi td").dblclick(function(){
+		parentIndex=$(this).parent().index();
+		thisIndex=$(this).index();
+		var textim=$(this).html();
+		if (thisIndex!=0){
+			$(this).parent().addClass("kontrolclassi");
+			if($(this).hasClass("kontrolclassi")){}
+			else{
+				var namem=$(".pozunAnalizi tr:eq(0) th:eq("+ thisIndex +")").html();
+				namem = namem.replace(/ /g,'');//boşlukları sildim.
+				namem = namem.toLowerCase();//hepsini kucuk harf yaptım.
+				var laNamem=namem;
+				var latin = new Array("s","c","g","u","i","o"); 
+				var turkce = new Array("ş","ç","ğ","ü","ı","ö");
+				for (var i=0; i<=laNamem.length; i++){
+					for (var j=0;j<7;j++){
+						laNamem = laNamem.replace(turkce[j], latin[j]);
+					} 
+				} 
+				$(this).addClass("kontrolclassi");
+				$(this).css("padding","0px 0px 2px 0px");
+				$(this).css("color","#fff");
+				$(this).html("<input class='w100flLeft tablodaDuz' name='"+laNamem+"' type='text' value='"+textim+"' autofocus>");
+				//focusCik(textim,laNamem);
+			}
+		}
+		
+	});
+	//}
+	
+	// function focusCik(textim,laNamem) {
+	// 	$(".pozunAnalizi td").focusout(function(){
+	// 		$(this).html(textim);
+	// 		$(this).css("color","#000");
+	// 		$(this).css("padding","5px 10px");
+	//         $(this).removeClass("kontrolclassi");
+	//     });
+	// }
+
+	$( ".radiom" ).click(function(){
+		var deger=$( ".radiom:checked" ).val();
+		$(this).parent().nextAll().children("input").prop('disabled', false);
+		$(this).parent().prevAll().children("input").prop('disabled', false);
+		$(this).parent().parent().nextAll().children("td input").prop('disabled', true);
+		$(this).parent().parent().prevAll().children("td input").prop('disabled', true);
+	});
+	
 });
+
